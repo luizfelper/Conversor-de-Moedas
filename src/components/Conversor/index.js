0d702a8995a18cf214f3eee03aff57432e56fd23
Luiz Felipe <luizfelper@gmail.com>
2021-08-17 18:11:24 -0300

    Criar função para converter os valores FIXOS de EURO para BRL

--- src/components/Conversor/index.js
+++ src/components/Conversor/index.js
@@ -19,26 +19,28 @@ export default class Conversor extends Component {
         let url = `https://economia.awesomeapi.com.br/json/daily/${de_para}/${qtd_dias}`;
 
 
-        fetch(url)
+/*         fetch(url)
         .then(function (resposta) {
             resposta.json()
             .then(function (data) {
                 console.log(data);
-                //let moedaB_valor = (this.state.moedaA_valor * this.state.moedaB_val);
+             let cotacao = json[de_para];
+                let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
+                this.setState({moedaB_valor}) 
             })
-        })
+        }) */
 
-        /* 
-        fetch(url)
-        .then(res=>{
-            return res.json()
-            console.log(res.json)
+        
+     fetch(url)
+        .then(resposta=>{
+            return resposta.json()
         })
-        .then(json=>{
-            let cotacao = json[de_para].value;
+        .then(respostaDoServidor=>{
+            console.log(respostaDoServidor);
+            let cotacao = 6.12;
             let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
             this.setState({moedaB_valor})
-        }) */
+        }) 
     }
 
     render() {
@@ -46,16 +48,14 @@ export default class Conversor extends Component {
             <div className="conversor">
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
                 <input type="text"
-                    placeholder="Digite um valor"
+                    placeholder="Digite um valor em Euro"
                     onChange={(event)=>{this.setState({moedaA_valor: event.target.value})}}>
                 </input>
                     <button
-                    className="btnConverter"
-                    value="Converter"
-                    onClick={this.converter}>
+                    type="button" onClick={this.converter}>
                         Converter
                     </button>
-                <h2>{this.state.moedaB_valor}</h2>
+                <h2>{this.state.moedaB_valor} Reais</h2>
             </div>
         )
     }
