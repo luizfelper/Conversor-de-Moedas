508f04dd77ed688bd4fec284bd876f8e1581ba1d
Luiz Felipe <luizfelper@gmail.com>
2021-08-17 19:18:01 -0300

    Adicionar funçã Fetch do Javascript puro ao invés do React

--- src/components/Conversor/index.js
+++ src/components/Conversor/index.js
@@ -14,33 +14,16 @@ export default class Conversor extends Component {
     }
 
     converter(){
-        let de_para = `${this.props.moedaA}-${this.props.moedaB}`;
-        let qtd_dias = `1`; //Quantidade de dias a ser mostrado.
-        let url = `https://economia.awesomeapi.com.br/json/daily/${de_para}/${qtd_dias}`;
-
-
-/*         fetch(url)
-        .then(function (resposta) {
-            resposta.json()
-            .then(function (data) {
-                console.log(data);
-             let cotacao = json[de_para];
-                let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
-                this.setState({moedaB_valor}) 
-            })
-        }) */
-
-        
-     fetch(url)
-        .then(resposta=>{
-            return resposta.json()
-        })
-        .then(respostaDoServidor=>{
-            console.log(respostaDoServidor);
-            let cotacao = 6.12;
-            let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
-            this.setState({moedaB_valor})
-        }) 
+       fetch('https://economia.awesomeapi.com.br/json/daily/USD-BRL/1')
+       .then(function (response) {
+           return response.json();
+       })
+       .then(function(data){
+           console.log(data[0].high);
+           let cotacao = data[0].high;
+           let moedaB_valor = this.state.moedaA_valor * cotacao;
+           this.setState({moedaB_valor});
+       })
     }
 
     render() {
